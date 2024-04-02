7126c03365d806959d4747d5d44f2393192e972c
jd_786 <jaydeep_shil@certree.com>
2024-04-02 20:21:28 +0530

    Add project files.

---
+++ Minimum Number of Swaps to Make String Balanced/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Minimum_Number_of_Swaps_to_Make_String_Balanced
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Solution
+    {
+        public int MinSwaps(string s)
+        {
+            // https://www.youtube.com/watch?v=3YDBT9ZrfaU
+            int closingBrackets = 0;
+            int min = 0;
+            foreach (char c in s)
+            {
+                if (c == '[')
+                {
+                    closingBrackets--;
+                }
+                else if (c == ']')
+                {
+                    closingBrackets++;
+                }
+                min = Math.Max(closingBrackets, min);
+            }
+
+            return (min + 1) / 2;
+        }
+    }
+}
